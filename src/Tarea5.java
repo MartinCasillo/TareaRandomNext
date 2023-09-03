925cfb80bb3667b79670d5e8ab8ec9557ba9fad7
Martin Casillo <casillomartin29@gmail.com>
2023-09-03 16:16:44 -0300

    subiendo ejercicios

---
+++ src/Tarea5.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.util.Scanner;
+
+public class Tarea5 {
+    public static void main(String[] args){
+        int numeroIngresado = 0;
+        Random ran = new Random();
+        int nxt = ran.nextInt(10) + 1;
+        int contador = 0;
+
+        do {
+            System.out.println("Adivine el numero : ");
+            Scanner scan = new Scanner(System.in);
+            numeroIngresado = scan.nextInt();
+            contador ++;
+            if(numeroIngresado == nxt){
+                System.out.println("Ganaste !!");
+            }
+        }while(contador < 3);
+    }
+}
